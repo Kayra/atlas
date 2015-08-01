40d8d04c6f06a54a26295cabf7c194fde814332d
Kayra <KayraAlat@gmail.com>
2015-08-01 22:51:27 +0300

    Added user authentication to skills views

--- atlas/skills/views.py
+++ atlas/skills/views.py
@@ -1,34 +1,55 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # from .forms import UserForm
 from .models import Skill, Task, Days
 
 
 def skillProgressView(request):
-    # Pull all tasks for user
-    # Pull all skills for user
-    # Run progress utility methods
-    # Return object and pass it to the template to be displayed
-    return render(request, 'skills/skill_progress.html')
+
+    if request.user.is_authenticated():
+        # Pull all tasks for user
+        # Pull all skills for user
+        # Run progress utility methods
+        # Return object and pass it to the template to be displayed
+        return render(request, 'skills/skill_progress.html')
+
+    else:
+        return redirect('account_login')
 
 
 def skillOverview(request):
-    # Pull all tasks for user
-    # Pull all skills for user
-    # Pass them to the template to be displayed
-    return render(request, 'skills/skill_overview.html')
+
+    if request.user.is_authenticated():
+        # Pull all tasks for user
+        # Pull all skills for user
+        # Pass them to the template to be displayed
+        return render(request, 'skills/skill_overview.html')
+
+    else:
+        return redirect('account_login')
 
 
 def skillSetupView(request):
-    # Save one skill for user
-    # Save multiple tasks for user
-    return render(request, 'skills/skill_setup.html')
+
+    if request.user.is_authenticated():
+        # Save one skill for user
+        # Save multiple tasks for user
+        return render(request, 'skills/skill_setup.html')
+
+    else:
+        return redirect('account_login')
+
 
 def skillListView(request):
-    # Pull all tasks for user
-    # Run list utility methods
-    # Return dictionary containing tasks and times to be displayed
-    return render(request, 'skills/skill_list.html')
+
+    if request.user.is_authenticated():
+        # Pull all tasks for user
+        # Run list utility methods
+        # Return dictionary containing tasks and times to be displayed
+        return render(request, 'skills/skill_list.html')
+
+    else:
+        return redirect('account_login')
