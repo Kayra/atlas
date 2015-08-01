a925ab77ea7cfde5d5f7006ed8f933bb22873955
Kayra <KayraAlat@gmail.com>
2015-08-01 22:30:34 +0300

    Added basic admin functionality

--- atlas/skills/admin.py
+++ atlas/skills/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django import forms
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import Skill, Task, Days
+
+admin.register(Skill, Task, Days)(admin.ModelAdmin)
