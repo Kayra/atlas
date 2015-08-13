522f6cdb0c2c281d729a34c7cf278275197a1f91
Kayra <KayraAlat@gmail.com>
2015-08-13 09:26:41 +0300

    Added relevant API imports and JSON renderer to views

--- atlas/skills/views.py
+++ atlas/skills/views.py
@@ -2,6 +2,12 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+from .serializers import SkillSerializer, TaskSerializer, DaysSerializer
 
 from .models import Skill, Task, Days
 from .forms import SkillForm
@@ -71,3 +77,15 @@ def skillListView(request):
 
     else:
         return redirect('account_login')
+
+
+# API #########################################################################
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
